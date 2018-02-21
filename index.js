b68dfdea02d260bfc89976d65ac40d2236406a04
vincent <omegaortiz1997@gmail.com>
2018-02-21 19:04:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function destructivelyRemoveFirstKitten() {
 
 function appendKitten(name) {
   var john = [...kittens, name];
-  return ...kittens;
+  return kittens;
 }
 
 function prependKitten(name) {
