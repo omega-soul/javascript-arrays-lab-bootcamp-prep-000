2de551f5fbaf1499c1700c1c18d205eb4861b9c0
vincent <omegaortiz1997@gmail.com>
2018-02-21 18:54:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,9 +31,9 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
- return kittens.slice(1, kittens.length -2);
+ return kittens.slice(0, kittens.length -1);
 }
 
 function removeFirstKitten() {
-  return kittens.slice()
+  return kittens.slice();
 }
